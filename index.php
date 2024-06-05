4ccb4a8be106f6e239ebe3fa9e22911de61dd232
Louis Ouellet <louis_ouellet@hotmail.com>
2024-06-04 21:40:53 -0400

    Prepared the terrain for a GUI

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// Initiate Session
+if(session_status() !== PHP_SESSION_ACTIVE){
+    session_start();
+}
+
+//Import phpRouter class into the global namespace
+use LaswitchTech\phpRouter\phpRouter;
+
+if(!is_file(__DIR__ . '/webroot/index.php')){
+
+    //Load Composer's autoloader
+    require __DIR__ . "/vendor/autoload.php";
+
+    //Initiate phpRouter
+    $phpRouter = new phpRouter();
+}
+
+require __DIR__ . '/webroot/index.php';
