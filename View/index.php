3b89ea37abf15a17cdf73f4da3d4138eaf68379a
Louis Ouellet <louis_ouellet@hotmail.com>
2024-06-10 11:08:41 -0400

    Reworked the index to show a table of all the scans and offer maintenance.

--- View/index.php
+++ View/index.php
@@ -1,20 +1,5 @@
 
-<div class="my-3 card p-3">
-    <form method="GET">
-        <div class="input-group d-flex flex-nowrap w-100">
-            <label class="input-group-text" for="id-field">Select a Scan</label>
-            <select id="id-field" class="form-select" name="id">
-                <?php foreach($this->Return['scans'] as $scan): ?>
-                    <option value="<?= $scan['id']; ?>" <?php if(isset($_GET['id']) && $_GET['id'] == $scan['id']){ echo "selected"; } ?>><?= $scan['type']; ?>: <?= $scan['target']; ?>:<?= $scan['port']; ?></option>
-                <?php endforeach; ?>
-            </select>
-            <button class="btn btn-success" type="submit">Submit</button>
-        </div>
-    </form>
-</div>
-<div class="my-3 card p-3">
-    <canvas id="resultsChart" style="max-height:500px;"></canvas>
-</div>
+<div id="scansTable" class="my-3 card p-0"></div>
 <script>
     $(document).ready(function(){
 
@@ -27,48 +12,101 @@
             allowClear: true
         });
 
-        // Results Chart
-        const ctx = document.getElementById('resultsChart').getContext('2d');
-        const myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: <?php if(isset($this->Return['chart'],$this->Return['chart']['labels'])){ echo json_encode($this->Return['chart']['labels']); } else { echo '[]'; } ?>,
-                datasets: <?php if(isset($this->Return['chart'],$this->Return['chart']['datasets'])){ echo json_encode(array_values($this->Return['chart']['datasets'])); } else { echo '[]'; } ?>
-            },
-            options: {
-                scales: {
-                    x: {
-                        grid: {
-                            color: 'rgba(255, 255, 255, 1)' // Grid color
-                        },
-                        ticks: {
-                            color: 'rgba(255, 255, 255, 1)' // Labels text color
-                        }
-                    },
-                    y: {
-                        grid: {
-                            color: 'rgba(255, 255, 255, 1)' // Grid color
-                        },
-                        ticks: {
-                            color: 'rgba(255, 255, 255, 1)' // Labels text color
-                        }
-                    }
+        // Const for records
+        const Records = JSON.parse('<?= json_encode($this->Return['scans'],JSON_UNESCAPED_SLASHES); ?>');
+
+        // Component
+        const Table = builder.Component(
+            "table",
+            "#scansTable",
+            {
+                class: {
+                    buttons: "px-4 pt-4",
+                    table: "border-top",
+                    footer: "px-4 pt-2 pb-4",
                 },
-                plugins: {
-                    legend: {
-                        labels: {
-                            color: 'rgba(255, 255, 255, 1)', // Legend text color
-                            font: {
-                                size: 14,
-                                family: 'Arial',
-                                style: 'italic',
-                                weight: 'bold'
+                showButtonsLabel: true,
+                selectTools:false,
+                exportTools:true,
+                actions:{},
+                datatable:{
+                    columnDefs:[
+                        { target: 0, visible: true, responsivePriority: 1, title: 'Target', name: 'target', data: 'target' },
+                        { target: 1, visible: true, responsivePriority: 2, title: 'Port', name: 'port', data: 'port' },
+                        { target: 2, visible: true, responsivePriority: 3, title: 'Type', name: 'type', data: 'type' },
+                        { target: 3, visible: true, responsivePriority: 4, title: 'Status', name: 'status', data: 'status', render: function(data, type, row, meta){
+                            if(data){
+                                return '<button class="btn btn-sm btn-success" data-id="'+row.id+'" data-action="disable">Active</button>';
+                            } else {
+                                return '<button class="btn btn-sm btn-danger" data-id="'+row.id+'" data-action="enable">Inactive</button>';
+                            }
+                        }},
+                        { target: 4, visible: true, responsivePriority: 5, title: 'Action', name: 'delete', data: 'delete', render: function(data, type, row, meta){
+                            return '<button class="btn btn-sm btn-danger" data-id="'+data+'" data-action="delete">Delete</button>';
+                        }},
+                    ],
+                    buttons:[
+                        {
+                            text: '<i class="bi-plus-lg"></i>',
+                            action:function(e, dt, node, config){
+                                console.log(e, dt, node, config);
+                                dt.row.add(Records[builder.Helper.randomNumber(0,9)]).draw();
                             },
-                            borderColor: 'rgba(255, 255, 255, 1)' // Not directly applicable to labels, but showing how to set options
                         }
-                    }
-                }
+                    ],
+                },
             },
-        });
+            function(table,component){
+
+                // Add the records
+                for(const [key, record] of Object.entries(Records)){
+                    table.add(record);
+                }
+
+                // Handle actions
+                $('#scansTable').on('click','button',function(){
+                    const action = $(this).data('action');
+                    const id = $(this).data('id');
+                    const label = action.charAt(0).toUpperCase() + action.slice(1);
+                    var color = 'danger';
+                    switch(action){
+                        case 'enable':
+                            color = 'success';
+                            break;
+                    }
+                    var modal = builder.Component(
+                        "modal",
+                        $(this),
+                        {
+                            callback: {
+                                submit: function(element,modal){
+                                    window.location.href = element.footer.submit.attr('href');
+                                },
+                            },
+                            onEnter: true,
+                            close:false,
+                            fullscreen:true,
+                            destroy:true,
+                            icon: "info-circle",
+                            title: "Are you sure?",
+                            body: "Are you sure you want to continue?",
+                            static: false,
+                            cancel: true,
+                            submit: true,
+                            center: true,
+                            size: "none",
+                        },
+                        function(modal,component){
+                            component.footer.submit
+                                .addClass('text-bg-'+color+' rounded-start')
+                                .attr('style','border-top-left-radius: 0px!important;')
+                                .attr('href','?id='+id+'&action='+action)
+                                .text(label);
+                        },
+                    );
+                    modal.show();
+                });
+            },
+        );
     });
 </script>
