d619cf8aa9345ce5c1e6ea9389331ce83f5379b9
Louis Ouellet <louis_ouellet@hotmail.com>
2024-06-10 12:43:32 -0400

    Updated the information View file to include as much information as possible

--- View/info.php
+++ View/info.php
@@ -1,5 +1,19 @@
 <?php
 
+// Display the Router Information
+echo "<h1>ROUTER</h1>" . PHP_EOL . "<br>";
+echo "Namespace: " . $this->getNamespace() . PHP_EOL . "<br>";
+echo "Route: " . $this->getRoute() . PHP_EOL . "<br>";
+echo "Label: " . $this->getLabel() . PHP_EOL . "<br>";
+echo "Icon: " . $this->getIcon() . PHP_EOL . "<br>";
+echo "Color: " . $this->getColor() . PHP_EOL . "<br>";
+echo "Location: " . $this->getLocation() . PHP_EOL . "<br>";
+echo "View: " . $this->getView() . PHP_EOL . "<br>";
+echo "View File: " . $this->getViewFile() . PHP_EOL . "<br>";
+echo "Template: " . $this->getTemplate() . PHP_EOL . "<br>";
+echo "Template File: " . $this->getTemplateFile() . PHP_EOL . "<br>";
+echo PHP_EOL . "<br>";
+
 // If Session is started
 if(session_status() == PHP_SESSION_ACTIVE) {
     echo "<h1>SESSION</h1>" . PHP_EOL . "<br>";
